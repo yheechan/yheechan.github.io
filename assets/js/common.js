150833af21325f5c1ce5668dc52bd6a8f3bfd8b2
yheechan <heechy06@naver.com>
2025-11-12 14:02:43 +0900

    Update for equal contribution note

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,16 +1,25 @@
 $(document).ready(function () {
-  // add toggle functionality to abstract, award and bibtex buttons
+  // add toggle functionality to note, abstract, award and bibtex buttons
+  $("a.note").click(function () {
+    $(this).parent().parent().find(".note.hidden").toggleClass("open");
+    $(this).parent().parent().find(".abstract.hidden.open").toggleClass("open");
+    $(this).parent().parent().find(".award.hidden.open").toggleClass("open");
+    $(this).parent().parent().find(".bibtex.hidden.open").toggleClass("open");
+  });
   $("a.abstract").click(function () {
+    $(this).parent().parent().find(".note.hidden.open").toggleClass("open");
     $(this).parent().parent().find(".abstract.hidden").toggleClass("open");
     $(this).parent().parent().find(".award.hidden.open").toggleClass("open");
     $(this).parent().parent().find(".bibtex.hidden.open").toggleClass("open");
   });
   $("a.award").click(function () {
+    $(this).parent().parent().find(".note.hidden.open").toggleClass("open");
     $(this).parent().parent().find(".abstract.hidden.open").toggleClass("open");
     $(this).parent().parent().find(".award.hidden").toggleClass("open");
     $(this).parent().parent().find(".bibtex.hidden.open").toggleClass("open");
   });
   $("a.bibtex").click(function () {
+    $(this).parent().parent().find(".note.hidden.open").toggleClass("open");
     $(this).parent().parent().find(".abstract.hidden.open").toggleClass("open");
     $(this).parent().parent().find(".award.hidden.open").toggleClass("open");
     $(this).parent().parent().find(".bibtex.hidden").toggleClass("open");
